4931947910a1ca6609f4cc5491611942d5f4f047
Dino Srdoč <dsrdoc96@gmail.com>
2018-02-26 16:15:52 +0100

    implemented basic CRUD functionality as proof of concept

--- CalorieBurner/Daily.swift
+++ CalorieBurner/Daily.swift
@@ -13,4 +13,44 @@ class Daily: NSManagedObject {
     public class func makeFetchRequest() -> NSFetchRequest<Daily> {
         return NSFetchRequest<Daily>(entityName: "Daily")
     }
+    
+    public class func fetchRequest(in date: Date) -> NSFetchRequest<Daily> {
+        let request = NSFetchRequest<Daily>(entityName: "Daily")
+        request.predicate = isInSameDayPredicate(as: date)
+        
+        return request
+    }
+    
+    static let dateFormatter: DateFormatter = {
+        let fmt = DateFormatter()
+        fmt.dateFormat = "YYYY-MM-DD"
+        
+        return fmt
+    }()
+    
+    public class func isInSameDayPredicate(as date: Date) -> NSPredicate {
+        let newYear = Calendar.current.component(.year, from: date)
+        let newMonth = Calendar.current.component(.month, from: date)
+        let newDay = Calendar.current.component(.day, from: date)
+        
+        return NSPredicate(format: "year == %@ AND month == %@ AND day == %@", argumentArray: [newYear, newMonth, newDay])
+    }
+    
+    public var created: Date {
+        get {
+            let dateString = "\(year)-\(month)-\(day)"
+            return Daily.dateFormatter.date(from: dateString)!
+        }
+        set {
+            let newYear = Calendar.current.component(.year, from: newValue)
+            let newMonth = Calendar.current.component(.month, from: newValue)
+            let newDay = Calendar.current.component(.day, from: newValue)
+//            let newDate = Calendar.current.dateComponents([.year, .month, .day], from: newValue)
+            year = Int16(newYear)
+            month = Int16(newMonth)
+            day = Int16(newDay)
+            
+            print("\(year)-\(month)-\(day)")
+        }
+    }
 }
--- CalorieBurner/ViewController.swift
+++ CalorieBurner/ViewController.swift
@@ -7,9 +7,96 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    private enum DatabaseActionType { case add, delete, print }
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var massTextField: UITextField!
+    @IBOutlet weak var energyTextField: UITextField!
+    
+    let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let measurementFormatter: MeasurementFormatter = {
+        let fmt = MeasurementFormatter()
+        fmt.unitOptions = .providedUnit
+        fmt.unitStyle = .medium
+        
+        return fmt
+    }()
+    
+    private func performAction(_ type: DatabaseActionType, context: NSManagedObjectContext) {
+        switch type {
+        case .add:
+            if let massTextValue = massTextField.text,
+               let massValue = Double(massTextValue),
+               let energyTextValue = energyTextField.text,
+               let energyValue = Double(energyTextValue)
+            {
+                let mass = Measurement(value: massValue, unit: UnitMass.kilograms)
+                let energy = Measurement(value: energyValue, unit: UnitEnergy.kilocalories)
+                
+                print(mass)
+                print(energy)
+                
+                if let dailies = try? context.fetch(Daily.fetchRequest(in: datePicker.date)) as [Daily],
+                   let daily = dailies.first
+                {
+                    daily.mass = mass
+                    daily.energy = energy
+                    daily.created = datePicker.date
+                    daily.isFromHealthKit = false
+                } else {
+                    let daily = Daily(context: context)
+                    daily.mass = mass
+                    daily.energy = energy
+                    daily.created = datePicker.date
+                    daily.isFromHealthKit = false
+                }
+                
+                try? context.save()
+            }
+            
+        case .delete:
+            let requestDeletion = Daily.fetchRequest(in: datePicker.date)
+            if let dailies = try? context.fetch(requestDeletion) as [Daily],
+               let daily = dailies.first
+            {
+                context.delete(daily)
+                try? context.save()
+            }
+            
+        case .print:
+            let request = Daily.fetchRequest(in: datePicker.date)
+            if let dailies = try? context.fetch(request) as [Daily],
+               let daily = dailies.first
+            {
+                guard let mass = daily.mass, let energy = daily.energy else {
+                    return
+                }
+                print(Daily.dateFormatter.string(from: daily.created))
+                print(measurementFormatter.string(from: mass))
+                print(measurementFormatter.string(from: energy))
+            } else {
+                print("no data for ", Daily.dateFormatter.string(from: datePicker.date))
+            }
+        }
+    }
+    
+    @IBAction func performDatabaseAction(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "Add"?:
+            performAction(.add, context: viewContext)
+        case "Delete"?:
+            performAction(.delete, context: viewContext)
+        case "Print"?:
+            performAction(.print, context: viewContext)
+        default:
+            print("Unsupported action")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
