9505a9fe35d6f4cad58458ab3d23dda5556520a1
Dino Srdoč <dsrdoc96@gmail.com>
2018-02-28 13:06:34 +0100

    editing table view cells with mass/energy is now supported. saving that data, on the  other hand, is not

--- CalorieBurner/DailyTableViewCell.swift
+++ CalorieBurner/DailyTableViewCell.swift
@@ -8,73 +8,259 @@
 
 import UIKit
 
+protocol DailyCellDelegate: class {
+    func didCancelEditing(cell: DailyTableViewCell, item: DailyTableViewCell.ItemType)
+    func didEndEditing(cell: DailyTableViewCell, item: DailyTableViewCell.ItemType, value: Double)
+}
+
 class DailyTableViewCell: UITableViewCell {
+    enum ItemType { case mass, energy }
     
-    private let measurementFormatter: MeasurementFormatter = {
+    private static let measurementFormatter: MeasurementFormatter = {
         let fmt = MeasurementFormatter()
         fmt.unitOptions = .providedUnit
         fmt.unitStyle = .medium
-        
+
         return fmt
     }()
     
-    @IBOutlet weak private var massTextField: UITextField!
-    @IBOutlet weak private var massUnitLabel: UILabel!
-    @IBOutlet weak private var energyTextField: UITextField!
-    @IBOutlet weak private var energyUnitLabel: UILabel!
+    private static let numberFormatter: NumberFormatter = {
+        let fmt = NumberFormatter()
+        fmt.maximumFractionDigits = 2
+        fmt.numberStyle = .decimal
+
+        return fmt
+    }()
+    
+    // TODO: - make this not hardcoded
+    
+    private static let massMultipliers = [0.25, 0.5, 1, 2.5, 5, 10]
+    private static var massSelectedMultiplier: Int? {
+        didSet {
+            guard let multiplier = massSelectedMultiplier else { return }
+            massValues = [Double](stride(from: 40, through: 250, by: massMultipliers[multiplier]))
+        }
+    }
+    private static var massValues = [Double](stride(from: 40, through: 250, by: 1))
+
+    private static let energyMultipliers = [25, 50, 100, 250, 500]
+    private static var energySelectedMultiplier: Int? {
+        didSet {
+            guard let multiplier = energySelectedMultiplier else { return }
+            energyValues = [Int](stride(from: 1000, through: 15000, by: energyMultipliers[multiplier]))
+        }
+    }
+    private static var energyValues = [Int](stride(from: 1000, through: 15000, by: 100))
+
+    weak var cellDelegate: DailyCellDelegate?
+    
+    // TODO: - clean this up
+    
+    var massPickerView: UIPickerView? {
+        didSet {
+            guard let picker = massPickerView else { return }
+
+            picker.dataSource = self
+            picker.delegate = self
+            picker.showsSelectionIndicator = true
+
+            massTextField.inputView = picker
+        }
+    }
+
+    var massPickerToolbar: UIToolbar? {
+        didSet {
+            guard let toolbar = massPickerToolbar else { return }
+
+            toolbar.barStyle = .default
+            toolbar.isTranslucent = true
+            toolbar.sizeToFit()
+            toolbar.isUserInteractionEnabled = true
+
+            let cancelButton = UIBarButtonItem(
+                title: "Cancel",
+                style: .plain,
+                target: self,
+                action: #selector(didCancelEditing(_:))
+            )
+            let flexibleButton = UIBarButtonItem(
+                barButtonSystemItem: .flexibleSpace,
+                target: self,
+                action: nil
+            )
+            let nextButton = UIBarButtonItem(
+                title: "Next",
+                style: .done,
+                target: self,
+                action: #selector(setNextInput)
+            )
+            toolbar.setItems(
+                [cancelButton, flexibleButton, nextButton],
+                animated: false
+            )
+
+            massTextField.inputAccessoryView = toolbar
+        }
+    }
+
+    var energyPickerView: UIPickerView? {
+        didSet {
+            guard let picker = energyPickerView else { return }
+
+            picker.dataSource = self
+            picker.delegate = self
+            picker.showsSelectionIndicator = true
+
+            energyTextField.inputView = picker
+        }
+    }
+
+    var energyPickerToolbar: UIToolbar? {
+        didSet {
+            guard let toolbar = energyPickerToolbar else { return }
+
+            toolbar.barStyle = .default
+            toolbar.isTranslucent = true
+            toolbar.sizeToFit()
+            toolbar.isUserInteractionEnabled = true
+
+            let cancelButton = UIBarButtonItem(
+                title: "Cancel",
+                style: .plain,
+                target: self,
+                action: #selector(didCancelEditing(_:))
+            )
+            let flexibleButton = UIBarButtonItem(
+                barButtonSystemItem: .flexibleSpace,
+                target: self,
+                action: nil
+            )
+            let doneButton = UIBarButtonItem(
+                title: "Done",
+                style: .done,
+                target: self,
+                action: #selector(didEndEditing(_:))
+            )
+            toolbar.setItems(
+                [cancelButton, flexibleButton, doneButton],
+                animated: false
+            )
+
+            energyTextField.inputAccessoryView = toolbar
+        }
+    }
+    
+    @IBOutlet weak var massTextField: UITextField!
+    @IBOutlet weak var energyTextField: UITextField!
+    private var activeTextField: UITextField?
     
     /// Makes the input views display "No data"
     public func setEmpty() {
         massTextField.text = "No data"
         energyTextField.text = "No data"
-        massUnitLabel.text = nil
-        energyUnitLabel.text = nil
     }
     
-    // TODO: - make units modular, fix separating label and textfield
-    // maybe make the textfield's first responder something that isn't a keyboard?
-    // just make a stepper/picker that the user can choose their inputs with?
+    @objc private func setNextInput(_ sender: AnyObject) {
+//        guard let selectedRow = massPickerView?.selectedRow(inComponent: 1) else
+//        { return }
+//
+//        let value = DailyTableViewCell.massValues[selectedRow]
+//
+//        cellDelegate?.didEndEditing(cell: self, item: .mass, value: value)
+        didEndEditing(sender)
+    }
+    
+    @objc private func didCancelEditing(_ sender: AnyObject) {
+        resignFirstResponder()
+        endEditing(true)
+        print("canceled")
+    }
+    
+    @objc private func didEndEditing(_ sender: AnyObject) {
+        resignFirstResponder()
+        endEditing(true)
+        print("ended")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        massPickerView = UIPickerView()
+//        massPickerToolbar = UIToolbar()
+//
+//        energyPickerView = UIPickerView()
+//        energyPickerToolbar = UIToolbar()
+    }
+    
+    // TODO: - make units user selectable
     
     public var mass: Measurement<UnitMass>? {
-        get {
-            guard let massStringValue = massTextField.text,
-                  let massValue = Double(massStringValue)
-            else { return nil }
-            
-            return Measurement<UnitMass>(value: massValue, unit: .kilograms)
-        }
-        set {
-            if let mass = newValue {
-                massTextField.text = String(mass.value)
-                massUnitLabel.text = measurementFormatter.string(from: mass.unit)
+        didSet {
+            if let mass = mass {
+                massTextField.text = DailyTableViewCell.measurementFormatter.string(from: mass)
             }
         }
     }
     
     public var energy: Measurement<UnitEnergy>? {
-        get {
-            guard let energyStringValue = energyTextField.text, let energyValue = Double(energyStringValue) else {
-                return nil
-            }
-            
-            return Measurement<UnitEnergy>(value: energyValue, unit: .kilocalories)
-        } set {
-            if let energy = newValue {
-                energyTextField.text = String(energy.value)
-                energyUnitLabel.text = measurementFormatter.string(from: energy.unit)
+        didSet {
+            if let energy = energy {
+                energyTextField.text = DailyTableViewCell.measurementFormatter.string(from: energy)
             }
         }
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
+
+extension DailyTableViewCell: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == massPickerView {
+            return component == 0 ? DailyTableViewCell.massMultipliers.count : DailyTableViewCell.massValues.count
+        } else if pickerView == energyPickerView {
+            return component == 0 ? DailyTableViewCell.energyMultipliers.count : DailyTableViewCell.energyValues.count
+        }
+
+        return 0
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == massPickerView {
+            if component == 0 {
+                return DailyTableViewCell.numberFormatter.string(from: DailyTableViewCell.massMultipliers[row] as NSNumber)
+            }
+
+            return DailyTableViewCell.numberFormatter.string(from: DailyTableViewCell.massValues[row] as NSNumber)
+        } else if pickerView == energyPickerView {
+            if component == 0 {
+                return DailyTableViewCell.numberFormatter.string(from: DailyTableViewCell.energyMultipliers[row] as NSNumber)
+            }
+
+            return DailyTableViewCell.numberFormatter.string(from: DailyTableViewCell.energyValues[row] as NSNumber)
+        }
+
+        return "Pero kvržica" // wtf
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            if pickerView == massPickerView {
+                DailyTableViewCell.massSelectedMultiplier = row
+                massPickerView?.reloadComponent(1)
+            } else if pickerView == energyPickerView {
+                energyPickerView?.reloadComponent(1)
+                DailyTableViewCell.energySelectedMultiplier = row
+            }
+        } else if component == 1 {
+            if pickerView == massPickerView {
+                mass = Measurement<UnitMass>(value: DailyTableViewCell.massValues[row], unit: .kilograms)
+            } else if pickerView == energyPickerView {
+                energy = Measurement<UnitEnergy>(value: Double(DailyTableViewCell.energyValues[row]), unit: .kilocalories)
+            }
+        }
+    }
+}
+
+
--- CalorieBurner/DailyTableViewController.swift
+++ CalorieBurner/DailyTableViewController.swift
@@ -11,39 +11,33 @@ import CoreData
 
 class DailyTableViewController: UITableViewController {
     
-    private static let longDateFormatter: DateFormatter = {
+    private let longDateFormatter: DateFormatter = {
         let fmt = DateFormatter()
         fmt.dateFormat = "YYYY-MM-dd"
         
         return fmt
     }()
     
-    private static let prettyDateFormatter: DateFormatter = {
+    private let prettyDateFormatter: DateFormatter = {
         let fmt = DateFormatter()
         fmt.dateStyle = .long
         
         return fmt
     }()
     
-    private let startingDate: Date = {
-        return longDateFormatter.date(from: "2000-01-01")!
-    }()
-    private let endingDate: Date = {
-        return longDateFormatter.date(from: "2030-01-01")!
-    }()
+    private let startingDate = Calendar.current.date(from: DateComponents(year: 2000, month: 01, day: 01))!
+    private let endingDate = Calendar.current.date(from: DateComponents(year: 2030, month: 12, day: 31))!
     
-    private lazy var dayCount: Int = {
-        return Calendar.current.dateComponents([.day], from: startingDate, to: endingDate).day!
-    }()
+    private lazy var dayCount = Calendar.current.dateComponents([.day], from: startingDate, to: endingDate).day!
     
     private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    /// Used to calculate indexPaths in a UICollectionView
+    // Used to calculate indexPaths in a UICollectionView
     private func indexPath(from date: Date) -> IndexPath? {
         guard date >= startingDate && date <= endingDate else {
             return nil
         }
-        
+
         // a year component is just that, a year component
         // e.g. '2018-02-10' has a year component '2018'
         // sections start at the startingDate's year component
@@ -52,31 +46,40 @@ class DailyTableViewController: UITableViewController {
         // the day component is the number of days passed since
         // that particular date. in the case of the first example
         // it would be '31+10', or '41'
-//        let yearsComponent = Calendar.current.dateComponents([.year], from: startingDate, to: date).year!
         let dayComponent = Calendar.current.dateComponents([.day], from: startingDate, to: date).day!
-        
+
         return IndexPath(row: 0, section: dayComponent)
     }
     
-    /// inverse of indexPath(from:)
+    private func section(from newDate: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: startingDate, to: newDate).day!
+    }
+    
+    private func date(from section: Int) -> Date {
+        return Calendar.current.date(byAdding: DateComponents(day: section), to: startingDate)!
+    }
+    
+    // inverse of indexPath(from:)
     private func date(from indexPath: IndexPath) -> Date? {
         guard indexPath.section <= numberOfSections(in: tableView) && indexPath.row <= tableView(tableView, numberOfRowsInSection: indexPath.section) else {
             return nil
         }
-        
+
         let componentsToBeAdded = DateComponents(day: indexPath.section)
         return Calendar.current.date(byAdding: componentsToBeAdded, to: startingDate)
     }
     
     private func scrollToItem(at date: Date, animated: Bool) {
-        guard let indexPath = indexPath(from: date) else { return }
-        
+        let indexPath = IndexPath(item: 0, section: section(from: date))
+
         tableView.scrollToRow(at: indexPath, at: .middle, animated: animated)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 100
+        tableView.sectionHeaderHeight = 44
         scrollToItem(at: Date(), animated: false)
     }
 
@@ -91,79 +94,56 @@ class DailyTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DailyCell", for: indexPath) as? DailyTableViewCell,
-            let date = date(from: indexPath)
-        else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DailyCell", for: indexPath) as? DailyTableViewCell
+        else { fatalError("nocell") }
         
-        if let dailies = try? viewContext.fetch(Daily.fetchRequest(in: date)),
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? DailyTableViewCell else { return }
+        
+        if let date = date(from: indexPath),
+           let dailies = try? viewContext.fetch(Daily.fetchRequest(in: date)),
            let daily = dailies.first
         {
-            print(daily.mass, daily.energy)
             cell.mass = daily.mass
             cell.energy = daily.energy
         } else {
             cell.setEmpty()
         }
+        
+//        let massPicker = UIPickerView()
+//        let massT = UIToolbar()
 
-        return cell
+        cell.massPickerView = UIPickerView()
+        cell.energyPickerView = UIPickerView()
+        cell.massPickerToolbar = UIToolbar()
+        cell.energyPickerToolbar = UIToolbar()
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        guard let date = date(from: IndexPath(row: 0, section: section)) else {
-            return nil
-        }
+//        return "kita"
         
-        return DailyTableViewController.prettyDateFormatter.string(from: date)
+        let date = Calendar.current.date(byAdding: .day, value: section, to: startingDate)!
+        
+        return prettyDateFormatter.string(from: date)
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 100
+//    }
+    
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected date: ", longDateFormatter.string(from: date(from: indexPath.section)))
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
