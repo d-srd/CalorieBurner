8118135c7cbbc0935f2bf3b0a65e1b1538879b21
Dino Srdoč <dsrdoc96@gmail.com>
2018-03-28 19:30:44 +0200

    add a basic settings view controller

--- CalorieBurner/User Settings/SettingsViewController.swift
+++ CalorieBurner/User Settings/SettingsViewController.swift
@@ -9,9 +9,77 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
+    @IBOutlet weak var firstDayOfWeekCell: UITableViewCell!
+    @IBOutlet weak var firstDayOfWeekLabel: UILabel!
+    
+    @IBOutlet weak var unitMassCell: UITableViewCell!
+    @IBOutlet weak var unitMassLabel: UILabel!
+    
+    @IBOutlet weak var unitEnergyCell: UITableViewCell!
+    @IBOutlet weak var unitEnergyLabel: UILabel!
+    
+    private var dayOfWeekSelectionViewController: UIAlertController!
+    private var massUnitSelectionViewController: UIAlertController!
+    private var energyUnitSelectionViewController: UIAlertController!
+    
+    private let measurementFormatter = MeasurementFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dayOfWeekSelectionViewController = {
+            let controller = UIAlertController(title: "First day of week", message: nil, preferredStyle: .actionSheet)
+            
+            func handler(_ action: UIAlertAction) {
+                print(action)
+                firstDayOfWeekLabel.text = action.title
+            }
+            
+            for daySymbol in ["Saturday", "Sunday", "Monday"] {
+                let action = UIAlertAction(title: daySymbol, style: .default, handler: handler)
+                controller.addAction(action)
+            }
+            
+            controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil) }))
+            
+            return controller
+        }()
+        
+        massUnitSelectionViewController = {
+            let controller = UIAlertController(title: "Mass unit", message: nil, preferredStyle: .actionSheet)
+            
+            func handler(_ action: UIAlertAction) {
+                print(action)
+                unitMassLabel.text = action.title
+            }
+            
+            for massUnitSymbol in [UnitMass.kilograms, .pounds, .stones].map(measurementFormatter.string) {
+                let action = UIAlertAction(title: massUnitSymbol, style: .default, handler: handler)
+                controller.addAction(action)
+            }
+            
+            controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil) }))
+            
+            return controller
+        }()
+        
+        energyUnitSelectionViewController = {
+            let controller = UIAlertController(title: "Energy unit", message: nil, preferredStyle: .actionSheet)
+            
+            func handler(_ action: UIAlertAction) {
+                print(action)
+                unitEnergyLabel.text = action.title
+            }
+            
+            for energyUnitSymbol in [UnitEnergy.kilocalories, .kilojoules].map(measurementFormatter.string) {
+                let action = UIAlertAction(title: energyUnitSymbol, style: .default, handler: handler)
+                controller.addAction(action)
+            }
+            
+            controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil) }))
+            
+            return controller
+        }()
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +87,26 @@ class SettingsViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        if indexPath == tableView.indexPath(for: firstDayOfWeekCell) {
+            present(dayOfWeekSelectionViewController, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath == tableView.indexPath(for: firstDayOfWeekCell) {
+            present(dayOfWeekSelectionViewController, animated: true, completion: nil)
+        } else if indexPath == tableView.indexPath(for: unitMassCell) {
+            present(massUnitSelectionViewController, animated: true, completion: nil)
+        } else if indexPath == tableView.indexPath(for: unitEnergyCell) {
+            present(energyUnitSelectionViewController, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.selectionStyle = .none
     }
     
 
